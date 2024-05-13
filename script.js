a933f1297a9e22e6ca956eb1f4fdeb9538d76a89
patovera <verapatricio9@gmail.com>
2024-05-12 22:30:35 -0300

    remove feature added and change status button

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ function addBookToLibrary() {
     const title = prompt("title of your book:");
     const author = prompt("author of your book:");
     const pages = prompt("pages of your book:");
-    const state = prompt("state of your book:");
+    let state = prompt("have you finished reading the book (yes/no only):");
+    while (state != "yes" && state != "no"){
+        state = prompt("have you finished reading the book (yes/no only):");
+    }
     const newBook = new Book(title,author,pages,state);
     myLibrary.push(newBook);
 }
@@ -23,26 +26,49 @@ function clearLibrary() {
         library.removeChild(library.firstChild);
     }
 }
-function displaylibrary(myLibrary){
+function displaylibrary(myLibrary){ 
     for(let i = 0; i < myLibrary.length; i++){
         const book = document.createElement("div");
         book.classList.add("book");
-        const title = document.createElement("h2");
-        title.classList.add("title");
+        const title = document.createElement("h1");
         title.textContent = myLibrary[i].title;
         book.appendChild(title);
         const author = document.createElement("h3");
-        author.classList.add("author");
         author.textContent = myLibrary[i].author;
         book.appendChild(author);
         const pages = document.createElement("p");
-        pages.classList.add("pages");
-        pages.textContent = myLibrary[i].pages + "pages";
+        pages.textContent = myLibrary[i].pages + " " + "pages";
         book.appendChild(pages);
+        const stateButton = document.createElement("button");
+        stateButton.classList.add("state-button");
+        stateButton.addEventListener("click", () => {
+            if (stateButton.classList.contains("active")){
+                stateButton.classList.remove("active");
+                myLibrary[i].state = "no";
+                state.textContent = "not read";
+            } else {
+                stateButton.classList.add("active");
+                myLibrary[i].state = "yes";
+                state.textContent = "read";
+            }
+        })
+        book.appendChild(stateButton);
         const state = document.createElement("p");
-        state.classList.add("state");
-        state.textContent = myLibrary[i].state;
+        if (myLibrary[i].state == "yes"){
+            state.textContent = "read";
+        } else {
+            state.textContent = "not read";
+        }
         book.appendChild(state);
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete");
+        deleteButton.textContent = "Delete";
+        book.appendChild(deleteButton);
+        deleteButton.addEventListener("click", () => {
+            myLibrary.splice(i,1);
+            clearLibrary();
+            displaylibrary(myLibrary);
+        });
         library.appendChild(book);
     }
 }
